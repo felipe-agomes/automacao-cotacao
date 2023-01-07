37ae321210c79486e265d4ab46c561927e681d72
felipe-almeida-gomes <falmeidagomes13@gmail.com>
2023-01-07 11:28:18 -0300

    Commit inicial da automação coração

---
+++ index.js
@@ -0,0 +1,89 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const cotacao = async () => {
+	const browser = await puppeteer.launch({ headless: false })
+	const page = await browser.newPage()
+	await page.goto('https://web.whatsapp.com/')
+
+	const waitThisAtt = 'text/Programar'
+	const groupWpp = await page.waitForSelector(waitThisAtt)
+	groupWpp.click()
+	const waitCotacao = '._22Msk'
+	await page.waitForSelector(waitCotacao)
+
+	const cotacaoList = await page.evaluate(async () => {
+		let filterParams = 0
+
+		const excluirCotacaoAntiga = (cotacao) => {
+			if (cotacao.includes('xxx')) {
+				filterParams++
+			}
+			if (filterParams === 0) {
+				return cotacao
+			}
+		}
+
+		const arrayToObject = (element) => {
+			const arrayPosition = () => {
+				const array = []
+				for (let i = 0; i < element.length; i++) {
+					if (element[i] == '\n') {
+						array.push(i)
+					}
+				}
+				return array
+			}
+
+			const arrayPositionN = arrayPosition()
+
+			const sliceIndex = (str, start) => {
+				return element.indexOf(str, start)
+			}
+
+			element = {
+				codFabrica: element.slice(
+					sliceIndex('\n', arrayPositionN[0]) + 19,
+					sliceIndex('\n', arrayPositionN[1])
+				),
+				modelo: element.slice(
+					sliceIndex('\n', arrayPositionN[1]) + 8,
+					sliceIndex('\n', arrayPositionN[2])
+				),
+				codInterno: element.slice(
+					sliceIndex('\n', arrayPositionN[2]) + 16,
+					sliceIndex('\n', arrayPositionN[3])
+				),
+				cliente: element.slice(sliceIndex('\n', arrayPositionN[3]) + 9),
+			}
+			return element
+		}
+
+		const nodeListCotacao = document.querySelectorAll(
+			'div.ItfyB._3nbHh > div._22Msk > div.copyable-text > div > span.i0jNr.selectable-text.copyable-text > span'
+		)
+		const arrayCotacao = [...nodeListCotacao]
+		const result = arrayCotacao
+			.map(({ textContent }) => {
+				return textContent
+			})
+			.reverse()
+			.filter(excluirCotacaoAntiga)
+			.map(arrayToObject)
+		return result
+	})
+	console.log(cotacaoList)
+	fs.writeFile(
+		'cotacao.json',
+		JSON.stringify(cotacaoList, null, 4),
+		(err) => {
+			if (err) throw new Error('algo de errado')
+
+			console.log('feito')
+		}
+	)
+
+	// page.close()
+}
+
+cotacao()
